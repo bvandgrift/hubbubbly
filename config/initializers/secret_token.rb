076dd9e9bf0df686436eb4f24a3397861102844c
Ben Vandgrift <somethingfamiliar@gmail.com>
2010-07-26 21:00:42 -0400

    getting started.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubbubbly::Application.config.secret_token = 'b7e57e4bf176ac56ae7b3b2b139c79705b4b09c4244ab669c4f3d08e5dac702b0c7b4fcaa21c53d75cae7e6d643966f8492873b3a5a7bfcf67117be79f021377'
