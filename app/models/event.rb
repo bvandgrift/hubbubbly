1943dce941564c13ebae675e7640e2a653612b95
Ben Vandgrift <ben@vandgrift.com>
2011-09-23 17:22:26 -0400

    keep on truckin.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :calendar_feed
 
-  scope :upcoming, where('at >= ?', Time.now()).limit(10)
+  scope :upcoming, where('at >= ?', Time.now()).order('at').limit(10)
   default_scope where('at >= ?', Time.now).order('at')
 end
