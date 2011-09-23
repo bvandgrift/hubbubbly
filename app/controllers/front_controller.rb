38dbdceae234bdd877a7cd1dda1055ad4c277f87
Ben Vandgrift <ben@vandgrift.com>
2011-09-23 17:27:34 -0400

    account for nils on the front page.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,9 +1,9 @@
 class FrontController < ApplicationController
 
   def index
-    @resources = RelatedResource.all
-    @events = Event.upcoming
-    @clippings = Clipping.all
+    @resources = RelatedResource.all || []
+    @events = Event.upcoming || []
+    @clippings = Clipping.all || []
   end
 
 end
