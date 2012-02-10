b301861dac77dce20b4eec8fcd7af93ffc775dd1
Ben Vandgrift and Flaviu Simihaian <pair@hubbubbly.com>
2012-02-10 19:00:33 +0000

    clean up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-#Hubbubbly::Application.routes.draw do
 Rails.application.routes.draw do
 
   mount_point = Hubbubbly::Engine.config.mount_at
-  puts "FUCKING MOUNT POINT: #{mount_point.inspect}"
-#  mount Hubbubbly::Engine => "/hub"
 
   scope(:path => mount_point) do
     resources :clippings, :only => [:show, :index], :path_prefix => mount_point
