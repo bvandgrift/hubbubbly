16e1925c61b23a0e5f5710d3916b28a77f25817d
Ben Vandgrift and Flaviu Simihaian <pair@hubbubbly.com>
2012-02-10 21:27:46 +0000

    adding script for starting server from gem root.

--- spec/sample/config/application.rb
+++ spec/sample/config/application.rb
@@ -1,14 +1,18 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'bundler'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+else
+  raise "You need to install bundler."
 end
 
+require 'rubygems'
 require 'hubbubbly'
 
 module Sample
